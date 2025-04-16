038a986cedd0a4d8a75f9bf76ac0efa1b19a983b
TayeburRahman <tayebrayhan101+@gmail.com>
2025-04-16 18:20:53 +0600

    updates all the codes 7-9.32

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,96 @@
+import mongoose, { Document, Schema, Model } from "mongoose";
+import { IUser } from "./user.interface";
+import { ENUM_MEAL_TYPE } from "../../../enums/user";
+
+const UserSchema = new Schema<IUser>(
+  {
+    authId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Auth",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    profile_image: {
+      type: String,
+      default: null,
+    },
+    phone_number: {
+      type: String,
+      default: null,
+    },
+    isPhoneNumberVerified: {
+      type: Boolean,
+      default: false,
+    },
+    street: {
+      type: String,
+      default: null,
+    },
+    neighborhood: {
+      type: String,
+      default: null,
+    },
+    mail_types: {
+      type: [String],
+      default: ['None'],
+      enum: ENUM_MEAL_TYPE,
+    },
+    relevant_dielary: {
+      type: [String],
+      default: null,
+      enum: ['Gluten-Free', 'Vegan', 'Vegetarian', 'Keto', 'Paleo'],
+    },
+    helgth_goal: {
+      type: [String],
+      enum: ["weight_loss", "muscle_gain", "maintain_weight"],
+      required: true
+    },
+    age: {
+      type: String,
+      default: null,
+    },
+    weight: {
+      type: String,
+    },
+    hight: {
+      type: String,
+    },
+    activety_lavel: {
+      type: String,
+    },
+    date_of_birth: {
+      type: Date,
+    },
+    amount: {
+      type: Number,
+      default: 0,
+    },
+    duration_time: {
+      type: Date,
+    },
+    subscription_status: {
+      type: String,
+      enum: ["Active", "None", "Expired"],
+      default: "None",
+    },
+    status: {
+      type: String,
+      enum: ["active", "deactivate"],
+      default: "active",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User: Model<IUser> = mongoose.model<IUser>("User", UserSchema);
+
+export default User;
